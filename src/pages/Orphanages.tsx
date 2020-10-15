a2c045f7f5ef63ae9bf24f9a9ab278e19c5ee2c7
Edson Bastos <edsonbastos2@hotmail.com>
2020-10-14 21:23:06 -0300

    Criado página de mapa

--- src/pages/Orphanages.tsx
+++ src/pages/Orphanages.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { FiPlus} from 'react-icons/fi';
+import { Map, TileLayer} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../img/map-marker.svg';
+import '../styles/pages/orphanages.css';
 
 function Orphanages() {
     return (
-        <h1>Hello World!</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="MapMarker"/>
+                    <h2>Escolha um Orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Ceará</strong>
+                    <span>Fortaleza</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-3.7055833,-38.5821546]}
+                zoom={15}
+                style={{width:'100%', height:'100%'}}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+        </div>
     )
 }
 
